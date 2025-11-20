648d6496624695e8bbb39d216fac33b2f1771c61
mhraju69 <drzraju@gmail.com>
2025-11-21 03:30:17 +0600

    fix send booking reminder

---
+++ Talkfusion/celery.py
@@ -0,0 +1,14 @@
+import os
+from celery import Celery
+
+# Django settings মডিউল লোড করা
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Talkfusion.settings')
+
+# Celery app create
+app = Celery('Talkfusion')
+
+# Django settings থেকে celery config load করা
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# সকল registered Django app থেকে tasks auto discover করা
+app.autodiscover_tasks()
