4108fe616ddfcbee617a1a455a2adfc372aaabd0
Default User <mhraju069@gmail.com>
2026-02-24 00:29:50 +0600

    add: setup apple login

--- Talkfusion/settings.py
+++ Talkfusion/settings.py
@@ -32,7 +32,8 @@ GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 WHATSAPP_CONFIG_ID=os.getenv("WHATSAPP_CONFIG_ID")
 IG_APP_ID=os.getenv("IG_APP_ID")
-IG_APP_SECRET=os.getenv("IG_APP_SECRET")
+IG_APP_SECRET=os.getenv('IG_APP_SECRET')
+APPLE_CLIENT_ID = os.getenv("APPLE_CLIENT_ID")
    
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7#$m=&$%3a_op)s4yr7qy2+424rs4#)2xi%9nj$in&0-*xqdmf'
